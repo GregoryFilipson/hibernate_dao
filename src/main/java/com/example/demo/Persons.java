0f4bd6e7f14fbd30708c5469ad9304682895d039
GregoryFilipson <alivenemo@gmail.com>
2021-04-05 08:35:57 +0300

    fixed Persons.java

--- src/main/java/com/example/demo/Persons.java
+++ src/main/java/com/example/demo/Persons.java
@@ -9,70 +9,24 @@ import javax.persistence.Id;
 @Entity
 public class Persons {
 
-    private String id;
-    private String name;
-    private String surname;
-    private int age;
-    private String phoneNumber;
-    private String cityOfLiving;
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     @Id
     @GeneratedValue
-    public String getId() {
-        return id;
-    }
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "city_of_living")
+    private String cityOfLiving;
 
     public Persons() {
     }
 
-    @Column(name = "name")
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Column(name = "surname")
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Column(name = "age")
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Column(name = "phone_number")
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Column(name = "city_of_living")
-    public String getCityOfLiving() {
-        return cityOfLiving;
-    }
-
-    public void setCityOfLiving(String cityOfLiving) {
-        this.cityOfLiving = cityOfLiving;
-    }
 
     @Override
     public String toString() {
